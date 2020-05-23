1c220f248333dcdbbf176103a046ce9063e24dbe
Volodymyr Sauliak <3429638@gmail.com>
2020-05-23 09:14:58 +0300

    includes edited

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,9 @@
 function toggle_menu() {
   var t = document.getElementsByClassName("toggle_icon");
   var nav = document.getElementById("navbar_top");
-  
+
   if (t[0].classList.contains('hidden')) {
-     t[0].classList.remove('hidden');
+    t[0].classList.remove('hidden');
     t[1].classList.add('hidden');
     t[1].classList.add('hidden_small');
     nav.classList.add('hidden_small');
@@ -16,10 +16,15 @@ function toggle_menu() {
   }
 }
 function close_price_descr() {
-  var d = document.getElementById("price_descr");
+  var d = document.getElementsByClassName("price_descr");
   d.classList.add('hidden');
 }
-function open_price_descr() {
-  var d = document.getElementById("price_descr");
-  d.classList.remove('hidden');
-}
+function price_descr(i) {
+
+  var d = document.getElementsByClassName("price_descr");
+  var m = document.getElementsByClassName("messengers");
+  var s = document.getElementsByClassName("arrow");
+  m[i].classList.toggle('hidden');
+  s[i].classList.toggle('rotate');
+  d[i].classList.toggle('hidden');
+};
